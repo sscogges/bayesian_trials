41af6f484dad34b3ec77e9a92f3785948a5ea7f5
Scott Coggeshall <scott.coggeshall@va.gov>
2020-11-06 23:44:34 -0800

    add heights data

--- r/plan.R
+++ r/plan.R
@@ -1,6 +1,7 @@
 plan <- drake_plan(
   
-  
+  heights =  round(rnorm(20, mean = 5.8, sd = .5), digits = 2),
+
   presentation = rmarkdown::render(input = knitr_in("presentation.Rmd"),
                                    output_file = file_out("presentation.html"))
   
